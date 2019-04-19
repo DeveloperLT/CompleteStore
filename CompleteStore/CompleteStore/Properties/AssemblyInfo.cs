fdd48da51384f2f7026a2c00655cc5fd6f02c437
DeveloperLT <lucas.tecnologia-developers@outlook.com>
2019-04-19 18:25:31 -0300

    First commit

---
+++ CompleteStore/CompleteStore/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CompleteStore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CompleteStore")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0d3b74c9-da10-4b45-b3fd-7be9d42ff4c4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CompleteStore/Controllers/CadastroController.cs
@@ -0,0 +1,94 @@
+﻿using CompleteStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CompleteStore.Controllers
+{
+    public class CadastroController : Controller
+    {
+        private static List<GrupoProduto> _listaGrupoProduto = new List<GrupoProduto>()
+        {
+            new GrupoProduto() { Id=1, Nome="Perfume X", Ativo=true}
+        };
+        
+        
+        // GET: Cadastro
+        [Authorize]
+        public ActionResult GProd()
+        {
+            return View(_listaGrupoProduto);
+        }
+        [Authorize]
+        public ActionResult Cat()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult SubCategoria()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult MarcProd()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult LArm()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult UnMed()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Prod()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Country()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult State()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult City()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Forn()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Profiles()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult Users()
+        {
+            return View();
+        }
+    }
+}
